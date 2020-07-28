9810fe84674cca067b975b99ca5d9b9452346cbb
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2020-07-28 16:20:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
-cd /root
+service(){
+cat > brook.service <<-EOF
+[Unit]
+Description=Brook(/etc/systemd/system/brook.service)
+After=network.target
+Wants=network-online.target
 
+[Service]
+Type=simple
+User=root
+ExecStart=/root/brook wsserver -l value:port -p passwd
+Restart=on-failure
+RestartSec=10s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+
+cd /root
 echo -e "0.  install brook      1.  remove brook"
 read -p "请选择（仅填数字）:" num
-
+   
 if [["${num}" == "0"]];then
-
     rm -rf /root/brook*
     wget https://github.com/txthinking/brook/releases/download/v20200701/brook
     chmod +x brook
 
-    cat > brook.service <<-EOF
-    [Unit]
-    Description=Brook(/etc/systemd/system/brook.service)
-    After=network.target
-    Wants=network-online.target
-
-    [Service]
-    Type=simple
-    User=root
-    ExecStart=/root/brook wsserver -l value:port -p passwd
-    Restart=on-failure
-    RestartSec=10s
-
-    [Install]
-    WantedBy=multi-user.target
-    EOF
+    service
 
     read -p "请输入监听地址（default:0.0.0.0）:" value
     [[ -z ${value} ]] && value="0.0.0.0"
