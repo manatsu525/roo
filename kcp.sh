846546ff7022926df8a3b07d565ed2bd88511cc6
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2020-12-16 13:30:55 +0800

    Create kcp.sh

---
+++ kcp.sh
@@ -0,0 +1,24 @@
+cat > kcp <<-EOF
+{
+    "key": "Cjh19960525",
+    "crypt": "salsa20",
+    "mode": "fast2",
+    "mtu" : 990,
+    "sndwnd": 2048,
+    "rcvwnd": 2048,
+    "datashard": 10,
+    "parityshard": 3,
+    "dscp": 0,
+    "nocomp": true,
+    "acknodelay": false,
+    "nodelay": 0,
+    "interval": 20,
+    "resend": 0,
+    "nc": 0,
+    "sockbuf": 4194304,
+    "keepalive": 10,
+    "snmplog": "",
+    "snmpperiod": 60,
+    "tcp": false
+}
+EOF
