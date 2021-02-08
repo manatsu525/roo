a7cebf21606e139c375f75cb9b373632952c5bb4
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-02-08 20:27:24 +0800

    Create wssconfig.sh

---
+++ wssconfig.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd /root
+apt update -y
+apt install socat -y
+wget -qO- https://get.acme.sh|bash
+read -p "input domain:" dom
+/root/.acme.sh/acme.sh --issue -d ${dom} --standalone -k ec-256
+/root/.acme.sh/acme.sh --install-cert -d ${dom} --fullchain-file /root/plugin.crt --key-file /root/plugin.key --ecc
+cat > config.json <<EOF
+{
+"server":"0.0.0.0",
+"server_port":8443,
+"password":"sumire",
+"timeout":300,
+"method":"xchacha20-ietf-poly1305",
+"fast_open":false,
+"nameserver":"8.8.8.8",
+"mode":"tcp_and_udp",
+"plugin":"v2ray-plugin",
+"plugin_opts":"server;tls;host=${dom};cert=/etc/shadowsocks-libev/plugin.crt;key=/etc/shadowsocks-libev/plugin.key;path=/sumire;mux=0"
+}
+EOF
