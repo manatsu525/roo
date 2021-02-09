709af99f399b400d6ab0fd184631ab2a98e37d3f
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-02-09 17:49:12 +0800

    Update and rename caddyconf.sh to caddy.sh

---
+++ caddy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+service(){
+cat > caddy.service <<-EOF
+[Unit]
+Description=caddy(/etc/systemd/system/caddy.service)
+After=network.target
+Wants=network-online.target
+[Service]
+Type=simple
+User=root
+ExecStart=/root/caddy/caddy -agree=true -conf=/root/Caddyfile
+Restart=on-failure
+RestartSec=10s
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+cd /root
+read -p "input domain:" domain
+read -p "input v2ray_port:" v2ray_port
+cat > Caddyfile <<-EOF
+$domain {
+    tls lineair069@gmail.com
+    gzip
+	timeouts none
+    browse
+    root /var/lib/transmission-daemon/downloads
+    proxy /natsu 127.0.0.1:${v2ray_port} {
+        websocket
+    }
+}
+import sites/*
+EOF
+mkdir caddy
+cd caddy
+wget -O caddy.tar.gz https://github.com/manatsu525/v2ray/releases/download/v3.05/caddy_v1.0.4_linux_amd64.tar.gz
+tar -xzvf caddy.tar.gz
+service
+mv gost.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable caddy.service
+systemctl start caddy
