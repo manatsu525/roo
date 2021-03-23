03e41bf9837213901b341664a1459bc3a4fd2a47
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-03-23 15:35:12 +0800

    Update key.sh

--- key.sh
+++ key.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 read -p "input domain:" domain
-openssl genrsa -out server.key 4096
+openssl genrsa -out plugin.key 4096
 wait
-openssl req -new -x509 -days 3650 -key server.key -out server.crt -subj "/C=JP/ST=TOKYO/L=TOKYO/O=KUMA/OU=KUMA/CN=${domain}"
+openssl req -new -x509 -days 3650 -key plugin.key -out plugin.crt -subj "/C=JP/ST=TOKYO/L=TOKYO/O=KUMA/OU=KUMA/CN=${domain}"
 
