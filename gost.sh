b55dd8d3fa1151542526791a84741ed4baab0dc6
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-03-30 20:43:18 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -1,56 +1,67 @@
-service(){
-cat > gost.service <<-EOF
-[Unit]
-Description=gost(/etc/systemd/system/gost.service)
-After=network.target
-Wants=network-online.target
-[Service]
-Type=simple
-User=root
-ExecStart=/root/gost -L=ss+wss://AEAD_CHACHA20_POLY1305:passwd@:8443?path=/sumire
-Restart=on-failure
-RestartSec=10s
-[Install]
-WantedBy=multi-user.target
-EOF
-}
-
+#!/bin/bash
 
 cd /root
-echo -e "0.  install gost      1.  remove gost"
+cat > kcp <<-EOF
+{
+    "key": "tsukasakuro",
+    "crypt": "salsa20",
+    "mode": "fast",
+    "mtu" : 1200,
+    "sndwnd": 2048,
+    "rcvwnd": 2048,
+    "datashard": 30,
+    "parityshard": 15,
+    "dscp": 0,
+    "nocomp": true,
+    "acknodelay": false,
+    "nodelay": 0,
+    "interval": 20,
+    "resend": 0,
+    "nc": 0,
+    "sockbuf": 4194304,
+    "keepalive": 10,
+    "snmplog": "",
+    "snmpperiod": 60,
+    "tcp": false
+}
+EOF
+echo -e "0.ws  1.wss 2.ohttp 3.otls 4.kcp 5.remove"
 read -p "请选择（仅填数字）:" num
-   
-if [[ "${num}" == "0" ]];then
-    rm -rf /root/gost*
-    wget -O gost.gz "https://github.com/manatsu525/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz" && gzip -d gost.gz && chmod +x gost
 
-    service
+read -p "method:" value
+[[ -z ${value} ]] && value="wss"
 
-    read -p "method:" value
-    [[ -z ${value} ]] && value="wss"
+read -p "请输入端口（default:8443）:" port
+[[ -z ${port} ]] && port="8443"
 
-    read -p "请输入端口（default:8443）:" port
-    [[ -z ${port} ]] && port="8443"
+read -p "请输入密码:" passwd
+[[ -z ${passwd} ]] && passwd="sumire"
 
-    read -p "请输入密码:" passwd
-    [[ -z ${passwd} ]] && passwd="sumire"
-    
-    read -p "请输入path:" path
-    [[ -z ${path} ]] && path="sumire"
-    
-    sed -i "s/wss/${value}/g" gost.service
-    sed -i "s/8443/${port}/g" gost.service
-    sed -i "s/sumire/${path}/g" gost.service
-    sed -i "s/passwd/${passwd}/g" gost.service
+read -p "请输入path:" path
+[[ -z ${path} ]] && path="sumire"
 
-    mv gost.service /etc/systemd/system/
-    systemctl daemon-reload
-    systemctl enable gost.service
-    systemctl start gost
-    systemctl status gost
-elif [[ "${num}" == "1" ]];then
-    killall gost
-    systemctl stop gost
-    systemctl disable gost.service
-    rm -rf /root/gost* /root/install.log /etc/systemd/system/gost.service
-fi
+case ${num} in
+	0 ) echo "/root/gost -L=ss+ws://AEAD_CHACHA20_POLY1305:${passwd}@:${port}?path=${path} &" > shadow
+	;;
+	1 ) echo "/root/gost -L=ss+wss://AEAD_CHACHA20_POLY1305:${passwd}@:${port}?path=${path} &" > shadow
+	;;
+	2 ) echo "/root/gost -L=ss+ohttp://AEAD_CHACHA20_POLY1305:${passwd}@:${port}" > shadow
+	;;
+	3 ) echo "/root/gost -L=ss+otls://AEAD_CHACHA20_POLY1305:${passwd}@:${port}" > shadow
+	;;
+	4 ) echo "/root/gost -L=ss+kcp://AEAD_CHACHA20_POLY1305:${passwd}@:${port}?c=/root/kcp" > shadow
+	;;
+	5 ) update-rc.d -f shadow remove
+		rm /etc/init.d/shadow
+		rm shadow
+		rm -rf /root/gost*
+		killall gost
+		exit 0
+	;;
+esac
+
+wget -O gost.gz "https://github.com/manatsu525/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz" && gzip -d gost.gz && chmod +x gost
+chmod +x shadow
+cp shadow /etc/init.d/
+update-rc.d shadow defaults 90
+/etc/init.d/shadow >/dev/null &
