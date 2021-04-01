78b3864c4117d9bd115649f2f1583a87846b8f26
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-04-01 18:58:38 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -25,6 +25,25 @@ cat > kcp <<-EOF
     "tcp": false
 }
 EOF
+
+service(){
+cat > gost.service <<-EOF
+[Unit]
+Description=gost(/etc/systemd/system/gost.service)
+After=network.target
+Wants=network-online.target
+[Service]
+Type=simple
+User=root
+ExecStart=/root/gost wsserver -l value:port -p passwd --path /en
+Restart=on-failure
+RestartSec=10s
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+
 echo -e "0.ws  1.wss 2.ohttp 3.otls 4.kcp 5.remove"
 read -p "请选择（仅填数字）:" num
 
@@ -41,27 +60,29 @@ read -p "请输入path:" path
 [[ -z ${path} ]] && path="sumire"
 
 case ${num} in
-	0 ) echo "/root/gost -L=ss+ws://AEAD_CHACHA20_POLY1305:${passwd}@:${port}?path=${path} &" > shadow
+	0 ) shadow="/root/gost -L=ss+ws://AEAD_CHACHA20_POLY1305:${passwd}@:${port}?path=${path} &"
 	;;
-	1 ) echo "/root/gost -L=ss+wss://AEAD_CHACHA20_POLY1305:${passwd}@:${port}?path=${path} &" > shadow
+	1 ) shadow="/root/gost -L=ss+wss://AEAD_CHACHA20_POLY1305:${passwd}@:${port}?path=${path} &"
 	;;
-	2 ) echo "/root/gost -L=ss+ohttp://AEAD_CHACHA20_POLY1305:${passwd}@:${port}" > shadow
+	2 ) shadow="/root/gost -L=ss+ohttp://AEAD_CHACHA20_POLY1305:${passwd}@:${port}"
 	;;
-	3 ) echo "/root/gost -L=ss+otls://AEAD_CHACHA20_POLY1305:${passwd}@:${port}" > shadow
+	3 ) shadow="/root/gost -L=ss+otls://AEAD_CHACHA20_POLY1305:${passwd}@:${port}"
 	;;
-	4 ) echo "/root/gost -L=ss+kcp://AEAD_CHACHA20_POLY1305:${passwd}@:${port}?c=/root/kcp" > shadow
+	4 ) shadow="/root/gost -L=ss+kcp://AEAD_CHACHA20_POLY1305:${passwd}@:${port}?c=/root/kcp"
 	;;
-	5 ) update-rc.d -f shadow remove
-		rm /etc/init.d/shadow
-		rm shadow
-		rm -rf /root/gost*
-		killall gost
-		exit 0
+	5 ) rm -rf /root/gost*	
+	killall gost
+	systemctl stop gost
+	systemctl disable gost.service
+	rm -rf /etc/systemd/system/gost.service
+	exit 0
 	;;
 esac
 
+sed -i "/ExecStart/c ExecStart=${shadow}"
 wget -O gost.gz "https://github.com/manatsu525/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz" && gzip -d gost.gz && chmod +x gost
-chmod +x shadow
-cp shadow /etc/init.d/
-update-rc.d shadow defaults 90
-/etc/init.d/shadow >/dev/null &
+mv gost.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable gost.service
+systemctl start gost
+systemctl status gost
