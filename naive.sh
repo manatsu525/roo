ea662b4494d309db68f3e73033a900d485a77a10
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-04-03 16:25:18 +0800

    Create naive.sh

---
+++ naive.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+read -p "domain:" domain
+
+cd /root
+wget --no-check-certificate https://github.com/manatsu525/roo/releases/download/1/caddy && chmod +x caddy
+
+cat > Caddyfile <<-EOF
+:443, ${domain}
+tls lineair069@gmail.com
+route {
+  forward_proxy {
+    basic_auth sumire sumire
+    hide_ip
+    hide_via
+    probe_resistance
+  }
+  file_server { 
+    root /usr/downloads
+    browse 
+  }
+}
+
+./caddy start
