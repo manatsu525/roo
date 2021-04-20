a044cfb173d4d39c4cf0ce1d4f899ffec2c3f8c7
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-04-20 09:41:16 +0800

    Update sswss.sh

--- sswss.sh
+++ sswss.sh
@@ -13,6 +13,6 @@ cat > config.json <<EOF
 "nameserver":"8.8.8.8",
 "mode":"tcp_and_udp",
 "plugin":"v2ray-plugin",
-"plugin_opts":"server;tls;host=${dom};cert=/etc/shadowsocks-libev/plugin.crt;key=/etc/shadowsocks-libev/plugin.key;path=/sumire;mux=0"
+"plugin_opts":"server;tls;host=${dom};cert=/etc/shadowsocks-libev/plugin.crt;key=/etc/shadowsocks-libev/plugin.key;path=/natsu;mux=0"
 }
 EOF
