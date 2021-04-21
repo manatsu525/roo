f359ee457727f3b0e585a33b79b394648c21c5ed
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-04-21 23:38:41 +0800

    Update xtls.sh

--- xtls.sh
+++ xtls.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd /root
 cat > config.json <<-EOF
 {
     "log": {
@@ -19,7 +20,7 @@ cat > config.json <<-EOF
                 "decryption": "none",
                 "fallbacks": [
                     {
-                        "dest": 80 // 或者回落到其它也防探测的代理
+                        "dest": 80
                     }
                 ]
             },
@@ -45,3 +46,40 @@ cat > config.json <<-EOF
     ]
 }
 EOF
+
+service(){
+cat > caddy.service <<-EOF
+[Unit]
+Description=caddy(/etc/systemd/system/caddy.service)
+After=network.target
+Wants=network-online.target
+[Service]
+Type=simple
+User=root
+ExecStart=/root/caddy/caddy -agree=true -conf=/root/Caddyfile
+Restart=on-failure
+RestartSec=10s
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+cd /root
+read -p "input domain:" domain
+cat > Caddyfile <<-EOF
+${domain}:80 {
+    gzip
+	timeouts none
+    browse
+    root /usr/downloads
+}
+EOF
+mkdir caddy
+cd caddy
+wget -O caddy.tar.gz https://github.com/manatsu525/v2ray/releases/download/v3.05/caddy_v1.0.4_linux_amd64.tar.gz
+tar -xzvf caddy.tar.gz
+service
+mv caddy.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable caddy.service
+systemctl start caddy
