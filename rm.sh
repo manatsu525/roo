b7bc00392f5b7a6d8f524cd089791abdf2befad1
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-04-26 03:33:57 +0800

    Update rm.sh

--- rm.sh
+++ rm.sh
@@ -2,7 +2,9 @@
 
 systemctl stop trojan
 systemctl stop caddy
+systemctl stop xray
 systemctl disable trojan
 systemctl disable caddy
-rm -rf /etc/systemd/system/caddy.service /etc/systemd/system/trojan.service /root/caddy* /root/trojan* /root/Caddyfile /root/example
+systemctl disable xray
+rm -rf /etc/systemd/system/caddy.service /etc/systemd/system/trojan.service /etc/systemd/system/xray.service /root/caddy* /root/trojan* /root/xray* /root/Caddyfile /root/example
 systemctl daemon-reload
