702883c1900793d9f87f4ee31d776db99b43d029
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-05-02 10:30:59 +0800

    Create ecc.sh

---
+++ ecc.sh
@@ -0,0 +1,6 @@
+wget -O xray.zip https://github.com/manatsu525/roo/releases/download/1/Xray-linux-64.zip
+unzip xray.zip
+./xray tls cert -ca -domain=${domain} -name=KUMA -org=KUMA -expire=87600h -file=./
+openssl x509 -in _cert.pem -out plugin.crt
+openssl ec -in _key.pem -out plugin.key
+rm -rf xray* *.pem
