28f3db13541d8de87afff3bb5f94a13969d39cf3
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-05-02 10:35:47 +0800

    Update ecc.sh

--- ecc.sh
+++ ecc.sh
@@ -1,3 +1,4 @@
+read -p "input domain:" domain
 wget -O xray.zip https://github.com/manatsu525/roo/releases/download/1/Xray-linux-64.zip
 unzip xray.zip
 ./xray tls cert -ca -domain=${domain} -name=KUMA -org=KUMA -expire=87600h -file=./
