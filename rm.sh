2cb9ddc8e06df4518046ca5e676fd8157af56fb3
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-05-03 11:08:21 +0800

    Update rm.sh

--- rm.sh
+++ rm.sh
@@ -8,3 +8,5 @@ systemctl disable caddy
 systemctl disable xray
 rm -rf /etc/systemd/system/caddy.service /etc/systemd/system/trojan.service /etc/systemd/system/xray.service /root/caddy* /root/trojan* /root/xray* /root/Caddyfile /root/example
 systemctl daemon-reload
+apt purge nginx -y
+apt autoremove -y
