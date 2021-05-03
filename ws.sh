efcf256ab77a741c22a05e9ac41dfedc0cd6b322
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-05-03 11:46:45 +0800

    Update ws.sh

--- ws.sh
+++ ws.sh
@@ -34,32 +34,26 @@ cat > /etc/nginx/conf.d/default.conf <<-EOF
 server {
     ### 1:
     server_name ${domain};
-
     listen 80;
     rewrite ^(.*) https://\$server_name\$1 permanent;
     if (\$request_method  !~ ^(POST|GET)$) { return  501; }
     autoindex off;
     server_tokens off;
 }
-
 server {
     ### 2:
     ssl_certificate /root/plugin.crt;
-
     ### 3:
     ssl_certificate_key /root/plugin.key;
-
     ### 4:
     location /sub2
     {
         proxy_pass              http://127.0.0.1:${v2ray_port};
         proxy_redirect          off;
-
         proxy_http_version      1.1;
         proxy_set_header        Upgrade \$http_upgrade;
         proxy_set_header        Connection "upgrade";
         proxy_set_header        Host \$host;
-
         sendfile                on;
         tcp_nopush              on;
         tcp_nodelay             on;
@@ -68,24 +62,19 @@ server {
         proxy_buffering         off;
         proxy_buffer_size       8k;
     }
-
     listen 443 ssl http2;
     server_name \$server_name;
     charset utf-8;
-
     ssl_protocols TLSv1.2;
     ssl_ciphers ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK;
     ssl_prefer_server_ciphers on;
-
     ssl_session_cache shared:SSL:60m;
     ssl_session_timeout 1d;
     ssl_session_tickets off;
-
     ssl_stapling on;
     ssl_stapling_verify on;
     resolver 8.8.8.8 8.8.4.4 valid=300s;
     resolver_timeout 10s;
-
     # Security settings
     if (\$request_method  !~ ^(POST|GET)$) { return 501; }
     add_header X-Frame-Options DENY;
@@ -106,12 +95,12 @@ server {
         proxy_busy_buffers_size    64k; 
         proxy_temp_file_write_size 64k; 
      }
-        
+        
 	location /file 
         {
 	alias /usr/downloads;
-        autoindex on;            
-        autoindex_exact_size off;
+        autoindex on;            
+        autoindex_exact_size off;
         }
 }
 EOF
