8e7369828b0cb61e44c98fa2065ce5824a954a84
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-05-07 10:59:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ EOF
 
 
 cd /root
-echo -e "0.  install brook      1.  remove brook"
+echo -e "0.  install brook ws   1.install brook   2.  remove brook"
 read -p "请选择（仅填数字）:" num
    
 if [[ "${num}" == "0" ]];then
@@ -50,6 +50,33 @@ if [[ "${num}" == "0" ]];then
     systemctl start brook
     systemctl status brook
 elif [[ "${num}" == "1" ]];then
+    rm -rf /root/brook*
+    wget https://github.com/manatsu525/brook/releases/download/v20200501/brook
+    chmod +x brook
+
+    service
+
+    read -p "请输入监听地址（default:0.0.0.0）:" value
+    [[ -z ${value} ]] && value="0.0.0.0"
+
+    read -p "请输入端口（default:8080）:" port
+    [[ -z ${port} ]] && port="8080"
+
+    read -p "请输入密码:" passwd
+    [[ -z ${passwd} ]] && passwd="tsukasakuro"
+
+    sed -i "s/wsserver/server/g" brook.service
+    sed -i "s/value/${value}/g" brook.service
+    sed -i "s/port/${port}/g" brook.service
+    sed -i "s/passwd/${passwd}/g" brook.service
+    sed -i "s/--path \/en//g" brook.service
+
+    mv brook.service /etc/systemd/system/
+    systemctl daemon-reload
+    systemctl enable brook.service
+    systemctl start brook
+    systemctl status brook
+elif [[ "${num}" == "2" ]];then
     killall brook
     systemctl stop brook
     systemctl disable brook.service
