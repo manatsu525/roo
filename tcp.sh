1132689c8460891bf05dd627b05e6c6d30d0e199
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-05-13 09:01:49 +0800

    Create tcp.sh

---
+++ tcp.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+cd /root
+
+read -p "input v2ray_port:" v2ray_port
+
+cat > config.json <<-EOF
+{
+    "inbounds": [
+        {
+            "listen": "0.0.0.0",
+            "port": 1234,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "3e88bf4b-a1ab-4c36-bc83-ea7d263e5239"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+xray(){
+cat > xray.service <<-EOF
+[Unit]
+Description=xray(/etc/systemd/system/xray.service)
+After=network.target
+Wants=network-online.target
+[Service]
+Type=simple
+User=root
+ExecStart=/root/xray run -config /root/config.json
+Restart=on-failure
+RestartSec=10s
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+cd /root
+wget -O xray.zip https://github.com/manatsu525/roo/releases/download/1/Xray-linux-64.zip
+unzip xray.zip
+xray
+mv xray.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable xray.service
+systemctl start xray
