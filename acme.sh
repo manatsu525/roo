ee2196c261c9754118a105febf925cc01eaf04fb
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-05-22 15:26:19 +0800

    Update and rename wssconfig.sh to acme.sh

---
+++ acme.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /root
+apt update -y
+apt install socat -y
+wget -qO- https://get.acme.sh|bash
+read -p "input domain:" dom
+/root/.acme.sh/acme.sh --issue -d ${dom} --standalone -k ec-256
+/root/.acme.sh/acme.sh --install-cert -d ${dom} --fullchain-file /root/plugin.crt --key-file /root/plugin.key --ecc
