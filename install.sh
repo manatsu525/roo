25a5d10ad97e0f301b6ba49b99aa777e482dc5a2
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-07-28 13:24:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ EOF
 
 
 cd /root
-echo -e "0.  install brook ws   1.install brook   2.  remove brook"
+echo -e "0.  install brook ws   1.install brook   2.  remove brook   3.install brook wss"
 read -p "请选择（仅填数字）:" num
    
 if [[ "${num}" == "0" ]];then
@@ -81,4 +81,26 @@ elif [[ "${num}" == "2" ]];then
     systemctl stop brook
     systemctl disable brook.service
     rm -rf /root/brook* /root/install.log /etc/systemd/system/brook.service
+elif [[ "${num}" == "3" ]];then
+    rm -rf /root/brook*
+    wget https://github.com/manatsu525/brook/releases/download/v20200501/brook
+    chmod +x brook
+
+    service
+
+    read -p "请输入domain:" value
+    [[ -z ${value} ]] && value="manatsu26.tk"
+
+    read -p "请输入密码:" passwd
+    [[ -z ${passwd} ]] && passwd="tsukasakuro"
+
+    sed -i "s/wsserver/wssserver/g" brook.service
+    sed -i "s/-l value:port/--domain ${value}/g" brook.service
+    sed -i "s/passwd/${passwd}/g" brook.service
+
+    mv brook.service /etc/systemd/system/
+    systemctl daemon-reload
+    systemctl enable brook.service
+    systemctl start brook
+    systemctl status brook
 fi
