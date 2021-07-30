b48ea404967ccfb53ada43f5dd08f731a171115d
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-07-31 00:35:33 +0800

    Update acme.sh

--- acme.sh
+++ acme.sh
@@ -4,5 +4,7 @@ apt update -y
 apt install socat -y
 wget -qO- https://get.acme.sh|bash
 read -p "input domain:" dom
+
+/root/.acme.sh/acme.sh --register-account -m lineair069@gmail.com
 /root/.acme.sh/acme.sh --issue -d ${dom} --standalone -k ec-256
 /root/.acme.sh/acme.sh --install-cert -d ${dom} --fullchain-file /root/plugin.crt --key-file /root/plugin.key --ecc
