969dceaf07aa8f781a25dc7b02ede526bd72e476
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-11-30 00:54:21 +0800

    Create hysteria.sh

---
+++ hysteria.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+service(){
+cat > hysteria.service <<-EOF
+[Unit]
+Description=hysteria(/etc/systemd/system/hysteria.service)
+After=network.target
+Wants=network-online.target
+[Service]
+Type=simple
+User=root
+ExecStart=/root/hysteria/hysteria -config hysteria.json server
+Restart=on-failure
+RestartSec=10s
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+cd /root
+read -p "input domain:" domain
+read -p "input v2ray_port:" v2ray_port
+cat > hysteria.json <<-EOF
+{
+  "listen": ":${v2ray_port}",
+  "cert": "/root/plugin.crt",
+  "key": "/root/plugin.key",
+  "obfs": "tsukasakuro",
+  "up_mbps": 100,
+  "down_mbps": 100
+}
+EOF
+
+mkdir hysteria
+cd hysteria
+wget -O hysteria https://github.com/manatsu525/roo/releases/download/2/hysteria-linux-amd64
+service
+mv hysteria.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable hysteria.service
+systemctl start hysteria
