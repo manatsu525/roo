6cd7ccea0c656954774ec4cfc3fd0e35c6eedd6b
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-11-30 01:26:26 +0800

    Update rm.sh

--- rm.sh
+++ rm.sh
@@ -6,6 +6,10 @@ systemctl stop xray
 systemctl disable trojan
 systemctl disable caddy
 systemctl disable xray
+
+systemctl stop hysteria
+systemctl disable hysteria
+rm -rf /etc/systemd/system/hysteria.service /root/hysteria
 rm -rf /etc/systemd/system/caddy.service /etc/systemd/system/trojan.service /etc/systemd/system/xray.service /root/caddy* /root/trojan* /root/xray* /root/Caddyfile /root/example
 systemctl daemon-reload
 apt purge nginx -y
