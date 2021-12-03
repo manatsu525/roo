1b49d6595b7d64be728da54505989de36928bd0f
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-12-04 04:08:30 +0800

    Update mkcp.sh

--- mkcp.sh
+++ mkcp.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+cd /root
+
+read -p "input v2ray_port:" v2ray_port
+
 cat > config.json <<-EOF
 {
     "inbound": {
         "protocol": "vless",
         "listen": "0.0.0.0",
-        "port": 14100,
+        "port": ${v2ray_port},
         "settings": {
             "clients": [{
                     "id": "3e88bf4b-a1ab-4c36-bc83-ea7d263e5239",
@@ -38,3 +42,29 @@ cat > config.json <<-EOF
     }
 }
 EOF
+
+xray(){
+cat > xray.service <<-EOF
+[Unit]
+Description=xray(/etc/systemd/system/xray.service)
+After=network.target
+Wants=network-online.target
+[Service]
+Type=simple
+User=root
+ExecStart=/root/xray run -config /root/config.json
+Restart=on-failure
+RestartSec=10s
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+cd /root
+wget -O xray.zip https://github.com/manatsu525/roo/releases/download/1/Xray-linux-64.zip
+unzip xray.zip
+xray
+mv xray.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable xray.service
+systemctl start xray
