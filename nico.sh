7029fcfdcb3dac6cbeef6f9263eacf816f1ea5b9
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-12-17 16:04:20 +0800

    Update nico.sh

--- nico.sh
+++ nico.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+read -p "domain:" domain
+read -p "port:" port
+read -p "path:(default /natsu)" path
+[[ -z ${path} ]] && path="/natsu"
+[[ -z ${page} ]] && page="https://www.morinagamilk.co.jp"
+
+if [[ ! -e brook ]];then
+  wget https://github.com/manatsu525/roo/releases/download/1/nico
+  chmod +x ./nico
+fi
+
+./nico ${domain} ${page} ${domain}/${path} http://127.0.0.1:${port} ${domain}/file /usr/downloads &
