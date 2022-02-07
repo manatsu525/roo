3365987465ae3034459e11e1ccd6687210a266fc
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2022-02-07 13:15:04 +0800

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-service(){
-cat > caddy.service <<-EOF
-[Unit]
-Description=caddy(/etc/systemd/system/caddy.service)
-After=network.target
-Wants=network-online.target
-[Service]
-Type=simple
-User=root
-ExecStart=/root/caddy/caddy -agree=true -conf=/root/Caddyfile
-Restart=on-failure
-RestartSec=10s
-[Install]
-WantedBy=multi-user.target
-EOF
-}
-
 trojan(){
 cat > trojan.service <<-EOF
 [Unit]
@@ -39,32 +22,16 @@ read -p "input domain:" domain
 read -p "input v2ray_port:" v2ray_port
 read -p "input password(default: sumire):" passwd
 [[ -z ${passwd} ]] && passwd="sumire"
-cat > Caddyfile <<-EOF
-${domain}:80 {
-    redir https://${domain}{uri}
-}
-$domain:443 {
-    tls lineair069@gmail.com
-    gzip
-	timeouts none
-    browse
-    root /usr/downloads
-    proxy /natsu 127.0.0.1:${v2ray_port} {
-        websocket
-    }
-}
-import sites/*
-EOF
-mkdir caddy
-cd caddy
-wget -O caddy.tar.gz https://github.com/manatsu525/v2ray/releases/download/v3.05/caddy_v1.0.4_linux_amd64.tar.gz
-tar -xzvf caddy.tar.gz
-service
-mv caddy.service /etc/systemd/system/
-systemctl daemon-reload
-systemctl enable caddy.service
-systemctl start caddy
+export v2ray_port
+export domain
 
+read -p "cert type: 1.auto 2.self-signed" type
+case $type in
+    1) bash <(curl -L -s https://raw.githubusercontent.com/manatsu525/roo/master/caddya.sh) 
+    ;;
+    2) bash <(curl -L -s https://raw.githubusercontent.com/manatsu525/roo/master/caddy.sh) 
+    ;;
+esac
 
 cd /root
 wget https://github.com/manatsu525/v2ray-tcp-tls-web/releases/download/v1.2.1/trojan-go-linux-amd64.zip
