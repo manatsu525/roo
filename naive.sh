7d8102b19e11692f1695f9c54d5e07d5190d879c
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2022-07-27 11:30:28 +0800

    Update naive.sh

--- naive.sh
+++ naive.sh
@@ -3,7 +3,18 @@
 read -p "domain:" domain
 
 cd /root
-wget --no-check-certificate https://github.com/manatsu525/roo/releases/download/2/caddy && chmod +x caddy
+wget https://dl.google.com/go/go1.18.1.linux-amd64.tar.gz
+tar -zxvf go1.18.1.linux-amd64.tar.gz -C /usr/local/bin/
+
+echo "export GOROOT=/usr/local/bin/go" >> ~/.bashrc
+echo "export GOPATH=$HOME/go" >> ~/.bashrc
+echo "export PATH=$GOROOT/bin:$PATH" >> ~/.bashrc
+echo "export PATH=$PATH:$GOPATH/bin" >> ~/.bashrc
+echo "export GO111MODULE=on" >> ~/.bashrc
+source ~/.bashrc
+
+go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+~/go/bin/xcaddy build --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
 
 cat > Caddyfile <<-EOF
 :443, ${domain}
