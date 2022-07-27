eafb7a24b467fac2dcf077a03e7b8f02e0ee3810
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2022-07-27 16:10:52 +0800

    Update naive_install.sh

--- naive_install.sh
+++ naive_install.sh
@@ -3,15 +3,6 @@
 read -p "domain:" domain
 
 cd /root
-wget https://dl.google.com/go/go1.18.1.linux-amd64.tar.gz
-tar -zxvf go1.18.1.linux-amd64.tar.gz -C /usr/local/bin/
-
-echo "export GOROOT=/usr/local/bin/go" >> ~/.bashrc
-echo "export GOPATH=$HOME/go" >> ~/.bashrc
-echo "export PATH=$GOROOT/bin:$PATH" >> ~/.bashrc
-echo "export PATH=$PATH:$GOPATH/bin" >> ~/.bashrc
-echo "export GO111MODULE=on" >> ~/.bashrc
-source ~/.bashrc
 
 go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
 ~/go/bin/xcaddy build --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
