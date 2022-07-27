86e7ecc696981f7924bd3339e1cb17fb474ce126
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2022-07-27 16:32:32 +0800

    Update go_install.sh

--- go_install.sh
+++ go_install.sh
@@ -6,9 +6,9 @@ cd /root
 wget https://dl.google.com/go/go1.18.1.linux-amd64.tar.gz
 tar -zxvf go1.18.1.linux-amd64.tar.gz -C /usr/local/bin/
 
-echo "export GOROOT=/usr/local/bin/go" >> ~/.bashrc
-echo "export GOPATH=$HOME/go" >> ~/.bashrc
-echo "export PATH=/usr/local/bin/go/bin:$PATH" >> ~/.bashrc
-echo "export PATH=$PATH:$HOME/go/bin" >> ~/.bashrc
-echo "export GO111MODULE=on" >> ~/.bashrc
+echo 'export GOROOT=/usr/local/bin/go' >> ~/.bashrc
+echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+echo 'export PATH=$GOROOT/bin:$PATH' >> ~/.bashrc
+echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
+echo 'export GO111MODULE=on' >> ~/.bashrc
 source ~/.bashrc
