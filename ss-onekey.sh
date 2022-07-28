6e691d8f26b4f88db1bf9f8b989458c8fc374c54
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2022-07-28 16:07:30 +0800

    Update ss-onekey.sh

--- ss-onekey.sh
+++ ss-onekey.sh
@@ -40,22 +40,22 @@ case $type in
     3) echo "NO TLS"
     ;;
     4）cat > config.json <<-EOF
-    {
-    "inbound": {
-        "protocol": "shadowsocks",
-        "listen": "127.0.0.1",
-     "port": ${v2ray_port},
-     "settings": {
-        "email": "lineair069@gmail.com",
-        "method": "2022-blake3-chacha20-poly1305",
-        "password": "6xt9P+XsEdRkvVVZsPUg0v+cxt8rIztTXp1VQW2DJQ8=",
-        "level": 0,
-        "network": "tcp,udp"
-        }
-    },
-    "outbound": {"protocol": "freedom"}
+{
+"inbound": {
+    "protocol": "shadowsocks",
+    "listen": "127.0.0.1",
+ "port": ${v2ray_port},
+ "settings": {
+    "email": "lineair069@gmail.com",
+    "method": "2022-blake3-chacha20-poly1305",
+    "password": "6xt9P+XsEdRkvVVZsPUg0v+cxt8rIztTXp1VQW2DJQ8=",
+    "level": 0,
+    "network": "tcp,udp"
     }
-    EOF
+},
+"outbound": {"protocol": "freedom"}
+}
+EOF
 esac
 
 xray(){
