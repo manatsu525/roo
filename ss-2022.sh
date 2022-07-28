2c6238c6177ad5f4f21ea83aac2919788d07cb89
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2022-07-28 16:46:34 +0800

    Update ss-2022.sh

--- ss-2022.sh
+++ ss-2022.sh
@@ -6,13 +6,10 @@ cat > config.json <<-EOF
 {
 "inbound": {
     "protocol": "shadowsocks",
-    "listen": "0.0.0.0",
  "port": ${v2ray_port},
  "settings": {
-    "email": "lineair069@gmail.com",
     "method": "2022-blake3-chacha20-poly1305",
     "password": "6xt9P+XsEdRkvVVZsPUg0v+cxt8rIztTXp1VQW2DJQ8=",
-    "level": 0,
     "network": "tcp,udp"
     }
 },
