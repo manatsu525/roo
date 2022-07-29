018193094968eb3fbdb524d820dab7917d8f62b5
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2022-07-29 17:02:25 +0800

    Create tuic.sh

---
+++ tuic.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+read -p "domain:" domain
+
+cd /root
+wget --no-check-certificate -O tuic https://github.com/manatsu525/roo/releases/download/2/tuic-server-0.8.1-x86_64-linux-gnu
+
+cat > tuic.json <<-EOF
+{
+    "port": 443,
+    "token": "sumire",
+    "certificate": "/root/plugin.crt",
+    "private_key": "/root/plugin.key",
+
+    "ip": "0.0.0.0",
+    "congestion_controller": "cubic",
+    "max_idle_time": 15000,
+    "authentication_timeout": 1000,
+    "alpn": ["h3"],
+    "max_udp_relay_packet_size": 1500,
+    "log_level": "info"
+}
+EOF
+
+nohup ./tuic -c tuic.json &
