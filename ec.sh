611c27c2bc71a5c07bb3c1c7f44d3f3ff1fcbc35
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2022-07-30 16:42:22 +0800

    Update ec.sh

--- ec.sh
+++ ec.sh
@@ -1,7 +1,8 @@
 read -p "input domain:" domain
-wget -O xray.zip https://github.com/manatsu525/roo/releases/download/1/Xray-linux-64.zip
-unzip xray.zip
+
+[[ ! -a xray ]] && wget -O xray.zip https://github.com/manatsu525/roo/releases/download/1/Xray-linux-64.zip && unzip xray.zip
+
 ./xray tls cert -ca -domain=${domain} -name=KUMA -org=KUMA -expire=87600h -file=./
-openssl x509 -in _cert.pem -out plugin.crt
-openssl ec -in _key.pem -out plugin.key
-rm -rf xray* *.pem
+mv _cert.pem plugin.crt
+mv _key.pem plugin.key
+rm -rf *.pem
