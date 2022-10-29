2f30a82aeb7ea104da5d61c37fdfba863be567e6
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2022-10-29 12:16:36 +0800

    Update acme.sh

--- acme.sh
+++ acme.sh
@@ -6,5 +6,6 @@ wget -qO- https://get.acme.sh|bash
 read -p "input domain:" dom
 
 /root/.acme.sh/acme.sh --register-account -m lineair069@gmail.com
+/root/.acme.sh/acme.sh --set-default-ca  --server letsencrypt
 /root/.acme.sh/acme.sh --issue -d ${dom} --standalone -k ec-256
 /root/.acme.sh/acme.sh --install-cert -d ${dom} --fullchain-file /root/plugin.crt --key-file /root/plugin.key --ecc
