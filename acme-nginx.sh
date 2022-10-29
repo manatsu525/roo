2c99058ea8deae7dac5add45c33f5b9787d91f88
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2022-10-29 11:50:56 +0800

    Update acme-nginx.sh

--- acme-nginx.sh
+++ acme-nginx.sh
@@ -3,8 +3,6 @@ cd /root
 apt update -y
 apt install socat -y
 wget -qO- https://get.acme.sh|bash
-read -p "input domain:" dom
 
-/root/.acme.sh/acme.sh --register-account -m lineair069@gmail.com
-/root/.acme.sh/acme.sh --issue -d ${dom} --nginx -k ec-256
-/root/.acme.sh/acme.sh --install-cert -d ${dom} --fullchain-file /root/plugin.crt --key-file /root/plugin.key --ecc
+/root/.acme.sh/acme.sh --issue -d ${domain} --nginx -k ec-256
+/root/.acme.sh/acme.sh --install-cert -d ${domain} --fullchain-file /root/plugin.crt --key-file /root/plugin.key --ecc
