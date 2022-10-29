75a7aa764fdd056dac3ac3024c59545ae76836cd
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2022-10-29 11:53:38 +0800

    Update acme-nginx.sh

--- acme-nginx.sh
+++ acme-nginx.sh
@@ -6,3 +6,4 @@ wget -qO- https://get.acme.sh|bash
 
 /root/.acme.sh/acme.sh --issue -d ${domain} --nginx -k ec-256
 /root/.acme.sh/acme.sh --install-cert -d ${domain} --fullchain-file /root/plugin.crt --key-file /root/plugin.key --ecc
+systemctl restart nginx
