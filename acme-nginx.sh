78bd4b729d269b6e61f9f43f6259c730c2a9fa9b
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2022-10-30 10:59:29 +0800

    Update acme-nginx.sh

--- acme-nginx.sh
+++ acme-nginx.sh
@@ -4,8 +4,9 @@ apt update -y
 apt install socat -y
 wget -qO- https://get.acme.sh|bash
 
-/root/.acme.sh/acme.sh --register-account -m lineair069@gmail.com
 /root/.acme.sh/acme.sh --set-default-ca  --server letsencrypt
+
+/root/.acme.sh/acme.sh --register-account -m lineair069@gmail.com
 /root/.acme.sh/acme.sh --issue -d ${domain} --nginx -k ec-256
 /root/.acme.sh/acme.sh --install-cert -d ${domain} --fullchain-file /root/plugin.crt --key-file /root/plugin.key --ecc
 systemctl restart nginx
