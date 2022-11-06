614107b30bc4eae34e1d9e5a99047c7beb463fe0
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2022-11-06 10:31:31 +0800

    Update and rename ss-2022.sh to ss.sh

---
+++ ss.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+cd /root
+
+read -p "input v2ray_port:" v2ray_port
+export v2ray_port
+
+cat > config.json <<-EOF
+{
+"inbound": {
+    "protocol": "shadowsocks",
+ "port": ${v2ray_port},
+ "settings": {
+    "method": "xchacha20-poly1305",
+    "password": "sumire",
+    "network": "tcp,udp"
+    },
+"outbound": {"protocol": "freedom"}
+}
+EOF
+
+
+xray(){
+cat > xray.service <<-EOF
+[Unit]
+Description=xray(/etc/systemd/system/xray.service)
+After=network.target
+Wants=network-online.target
+[Service]
+Type=simple
+User=root
+ExecStart=/root/xray run -config /root/config.json
+Restart=on-failure
+RestartSec=10s
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+cd /root
+wget -O xray.zip https://github.com/manatsu525/roo/releases/download/1/Xray-linux-64.zip
+unzip xray.zip
+xray
+mv xray.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable xray.service
+systemctl start xray
