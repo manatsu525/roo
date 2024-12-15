c6ddbfe15726e411b591626f0694a387ef92dbf6
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2024-12-16 01:01:26 +0800

    Update tuic.sh

--- tuic.sh
+++ tuic.sh
@@ -7,18 +7,17 @@ wget --no-check-certificate -O tuic https://github.com/manatsu525/roo/releases/d
 cat > tuic.json <<-EOF
 {
     "server": "[::]:8443",
-    "token": ["sumire"],
+    
     "users": {
         "00000000-0000-0000-0000-000000000000": "sumire"
     },
     "certificate": "/root/plugin.crt",
     "private_key": "/root/plugin.key",
 
-    "congestion_controller": "bbr",
-    "max_idle_time": 15000,
-    "authentication_timeout": 1000,
+    "congestion_control": "bbr",
+    
     "alpn": ["h3", "spdy/3.1"],
-    "max_udp_relay_packet_size": 1500,
+    
     "log_level": "info"
 }
 EOF
