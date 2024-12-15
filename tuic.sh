7c3b775600b06ac721a4687a168862cffb3b61ec
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2024-12-16 00:54:45 +0800

    Update tuic.sh

--- tuic.sh
+++ tuic.sh
@@ -6,7 +6,7 @@ wget --no-check-certificate -O tuic https://github.com/manatsu525/roo/releases/d
 
 cat > tuic.json <<-EOF
 {
-    "port": 8443,
+    "server": "[::]:8443",
     "token": ["sumire"],
     "users": {
         "00000000-0000-0000-0000-000000000000": "sumire"
@@ -14,7 +14,6 @@ cat > tuic.json <<-EOF
     "certificate": "/root/plugin.crt",
     "private_key": "/root/plugin.key",
 
-    "ip": "::",
     "congestion_controller": "bbr",
     "max_idle_time": 15000,
     "authentication_timeout": 1000,
