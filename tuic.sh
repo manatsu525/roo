1390f08984c0a88c21a2c589ed821edd92265e49
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2024-12-16 00:32:17 +0800

    Update tuic.sh

--- tuic.sh
+++ tuic.sh
@@ -8,6 +8,9 @@ cat > tuic.json <<-EOF
 {
     "port": 8443,
     "token": ["sumire"],
+    "users": {
+        "00000000-0000-0000-0000-000000000000": "sumire"
+    },
     "certificate": "/root/plugin.crt",
     "private_key": "/root/plugin.key",
 
@@ -15,7 +18,7 @@ cat > tuic.json <<-EOF
     "congestion_controller": "bbr",
     "max_idle_time": 15000,
     "authentication_timeout": 1000,
-    "alpn": ["h3"],
+    "alpn": ["h3", "spdy/3.1"],
     "max_udp_relay_packet_size": 1500,
     "log_level": "info"
 }
