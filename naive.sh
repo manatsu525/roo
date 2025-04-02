17d45f6d69cc3bba96c01ce04ef54d79576f1c06
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2025-04-03 00:59:53 +0800

    Update naive.sh

--- naive.sh
+++ naive.sh
@@ -6,19 +6,32 @@ cd /root
 wget --no-check-certificate https://github.com/manatsu525/roo/releases/download/2/caddy && chmod +x caddy
 
 cat > Caddyfile <<-EOF
-:443, ${domain}
-tls lineair069@gmail.com
-route {
-  forward_proxy {
-    basic_auth sumire sumire
-    hide_ip
-    hide_via
-    probe_resistance
-  }
-  file_server { 
-    root /usr/downloads
-    browse 
-  }
+${domain}:443 {
+    tls lineair069@gmail.com
+
+    route {
+        forward_proxy {
+            basic_auth sumire sumire
+            hide_ip
+            hide_via
+            probe_resistance
+        }
+
+        handle_path /file/* {
+            file_server {
+                root /usr/downloads
+                browse
+            }
+        }
+
+        handle {
+            reverse_proxy www.honda.com {
+                header_up Host {http.request.host}
+                header_up X-Forwarded-For {http.request.remote.addr}
+                header_up X-Forwarded-Proto {http.request.scheme}
+            }
+        }
+    }
 }
 EOF
 
